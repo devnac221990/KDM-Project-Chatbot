fb013853e1e1260cee8c0a2dc0f2b1e005cf748d
Girees737 <muppallagiresh@gmail.com>
2021-05-04 00:25:52 -0400

    added code and screenshots
    added code and screenshots

---
+++ Source_code/training_UMKC_Bot.py
@@ -0,0 +1,17 @@
+from rasa_nlu.training_data import load_data
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+
+
+#MODELS1 = ['event_data']
+MODELS1 =["Tamak's_intents_data"] # ['event_data']
+
+#trainer = Trainer(config.load("configs/config_spacy.yml"))
+trainer = Trainer(config.load("configs/config_spacy_for_intent.yml"))
+for model_name in MODELS1:
+    print('Training for - {} model'.format(model_name))
+    training_data = load_data('training_data/data/{}.md'.format(model_name))
+    trainer.train(training_data)
+    model_directory = trainer.persist('./models/', fixed_model_name=model_name)
+
+print('model training finished.')
